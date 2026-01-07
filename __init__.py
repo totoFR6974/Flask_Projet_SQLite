f4bb1dc40ae98a085637e54bcbf27ccc2a72a9f5
toto_ <92277567+totoFR6974@users.noreply.github.com>
2026-01-07 14:57:47 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -79,3 +79,5 @@ def enregistrer_client():
                                                                                                                                        
 if __name__ == "__main__":
   app.run(debug=True)
+
+#
